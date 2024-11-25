935343d2f86930a98f75cd94de7aa0589b1d2000
An Ly <boptom@gmail.com>
2024-11-25 13:15:07 +1100

    Use flux on forgot password page

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -37,25 +37,30 @@ new #[Layout('layouts.guest')] class extends Component
 }; ?>
 
 <div>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+    <p class="text mb-6 text-sm">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+    </p>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form wire:submit="sendPasswordResetLink">
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        <flux:input
+            wire:model="email"
+            :label="__('Email')"
+            id="email"
+            style="font-size: 16px"
+            type="email"
+            name="email"
+            required
+            autofocus
+        />
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+        <div class="mt-4 flex items-center justify-end">
+            <flux:button type="submit" variant="primary" class="w-full">
                 {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+            </flux:button>
         </div>
     </form>
 </div>
