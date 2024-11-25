ca324bdabe138f6ba899a943beb3e89ee9968448
An Ly <boptom@gmail.com>
2024-11-25 14:49:47 +1100

    Create nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,108 @@
+<?php
+
+use App\Livewire\Actions\Logout;
+use Livewire\Volt\Component;
+
+new class extends Component
+{
+    /**
+     * Log the current user out of the application.
+     */
+    public function logout(Logout $logout): void
+    {
+        $logout();
+
+        $this->redirect('/', navigate: true);
+    }
+}; ?>
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link
+            href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
+            rel="stylesheet"
+        />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @fluxStyles
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <livewire:layout.navigation />
+    <body class="min-h-screen">
+        <flux:sidebar
+            sticky
+            stashable
+            class="border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900"
+        >
+            <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            <flux:brand
+                href="/"
+                logo="https://fluxui.dev/img/demo/logo.png"
+                :name="config('app.name')"
+                class="px-2 dark:hidden"
+            />
+            <flux:brand
+                href="/"
+                logo="https://fluxui.dev/img/demo/dark-mode-logo.png"
+                name="Flux AI"
+                class="hidden px-2 dark:flex"
+            />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+            <flux:input
+                as="button"
+                variant="filled"
+                placeholder="Search..."
+                icon="magnifying-glass"
+            />
+
+            <flux:navlist variant="outline"></flux:navlist>
+
+            <flux:spacer />
+
+            <flux:dropdown position="top" align="end">
+                <flux:profile name="{{ Auth::user()->name }}" />
+
+                <flux:navmenu>
+                    <flux:navmenu.item
+                        href="{{ route('profile') }}"
+                        icon="user"
+                    >
+                        Profile
+                    </flux:navmenu.item>
+
+                    <flux:menu.separator />
+
+                    @volt
+                        <flux:navmenu.item
+                            wire:click="logout"
+                            icon="arrow-right-start-on-rectangle"
+                        >
+                            Logout
+                        </flux:navmenu.item>
+                    @endvolt
+                </flux:navmenu>
+            </flux:dropdown>
+        </flux:sidebar>
+
+        <flux:header class="lg:hidden">
+            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+        </flux:header>
+
+        <!-- Page Content -->
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+
+        @fluxScripts
     </body>
 </html>
