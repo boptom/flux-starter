28478bc8fae41a398ec6cabf07177e41e903d796
An Ly <boptom@gmail.com>
2024-11-25 13:35:23 +1100

    Use flux on verify email

--- resources/views/livewire/pages/auth/verify-email.blade.php
+++ resources/views/livewire/pages/auth/verify-email.blade.php
@@ -36,23 +36,34 @@ new #[Layout('layouts.guest')] class extends Component
 }; ?>
 
 <div>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+    <div class="text mb-6 text-sm">
         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+        <div
+            class="mb-4 text-sm font-medium text-green-600 dark:text-green-400"
+        >
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
         </div>
     @endif
 
-    <div class="mt-4 flex items-center justify-between">
-        <x-primary-button wire:click="sendVerification">
+    <div class="mt-6 space-y-2">
+        <flux:button
+            variant="primary"
+            wire:click="sendVerification"
+            class="w-full"
+        >
             {{ __('Resend Verification Email') }}
-        </x-primary-button>
+        </flux:button>
 
-        <button wire:click="logout" type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+        <flux:button
+            wire:click="logout"
+            type="submit"
+            variant="ghost"
+            class="w-full"
+        >
             {{ __('Log Out') }}
-        </button>
+        </flux:button>
     </div>
 </div>
